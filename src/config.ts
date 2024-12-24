63f0815d9b6c4bb1561d868fa429a12ae2c10efd
torrentialmeowpour <jpsanders91@gmail.com>
2024-12-23 16:16:39 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,11 @@
 import 'dotenv/config';
 
-
 type Config = {
     rpcUrl: string;
     chainId: number;
     address: string;
 }
 
-const testnetConfig: Config = {
-    rpcUrl: 'https://1rpc.io/base-goerli',
-    chainId: , // add testnet 
-    address: '' // add testnet 
-}
 
 const mainnetConfig: Config = {
     rpcUrl: 'https://1rpc.io/base', //Rate limited and not for production systems.
@@ -19,10 +13,11 @@ const mainnetConfig: Config = {
     address: '0x5Dc939df0cf253873cf77B786A24d51BaA72cF75' // Taurus GA
 }
 
+
 export const getConfig = () => {
     if (process.env.TYPE_NETWORK === 'mainnet') {
-        return mainnetConfig;
+      return mainnetConfig
+    } else {
+        throw new Error("Please provide a valid TYPE_NETWORK in the env file.")
     }
-
-    return testnetConfig;
-}
+  };
